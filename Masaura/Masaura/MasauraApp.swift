48a8752ae6774a03b3bacf23b419d34d74b82dc0
Roan Bahri <23mirb@gmail.com>
2024-08-29 13:50:13 -0700

    Initial commit with new template

---
+++ Masaura/Masaura/MasauraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasauraApp.swift
+//  Masaura
+//
+//  Created by Roan Bahri on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MasauraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
